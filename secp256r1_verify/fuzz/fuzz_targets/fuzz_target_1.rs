4eb6e765a97e122cc0b5586ad4841fd5d44d5730
Iceomatic <89707822+iceomatic@users.noreply.github.com>
2023-12-10 23:24:04 +0100

    rm: solana_sdk from deps & update fuzz target

--- secp256r1_verify/fuzz/fuzz_targets/fuzz_target_1.rs
+++ secp256r1_verify/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -2,9 +2,16 @@
 
 use libfuzzer_sys::fuzz_target;
 use secp256r1_verify::utils::secp256r1_instruction::verify;
+use hex;
 
 fuzz_target!(|data: &[u8]| {
     // Call the verify function with the fuzzed data
-    let _ = verify(data, &[&[0u8; 100]]);
-    let _ = verify(data, &[&data]);
+    let result1 = verify(data, &[&[0u8; 100]]);
+    let result2 = verify(data, &[&data]);
+
+    // If num_sigs is not 0, the the result should be an error
+    if data.len() > 0 && data[0] != 0 {
+        assert!(result1.is_err());
+        assert!(result2.is_err());
+    }
 });
